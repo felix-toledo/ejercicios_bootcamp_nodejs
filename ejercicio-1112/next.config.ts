0ed750dee3597151236e170c9ee59555631559b5
Felix Toledo <felixtoledoctes@gmail.com>
2025-12-11 18:51:45 -0300

    ejercicios

--- ejercicio-1112/next.config.ts
+++ ejercicio-1112/next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Opciones de configuración de imágenes
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        port: "", // Se deja vacío si no se usa un puerto específico (como es el caso)
+        pathname: "/img/**", // Esto permite cualquier ruta dentro de /img/
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
